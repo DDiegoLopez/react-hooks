b50853ddafcdaa2df8f3681b5a62593445c0e97c
Diego-D <89713249+DDiegoLopez@users.noreply.github.com>
2023-08-18 21:05:07 -0600

    listo

---
+++ src/Form/DatosUsuario/index.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { TextField, Button, Box } from "@mui/material";
+
+const DatosUsuario = () => {
+
+  const [ email, setEmail ] = useState({value: '', valid: true})
+  const [ password, setPassword ] = useState({value: '', valid: true})
+
+  //constructor(props) {
+    //super(props)
+    //this.state = {
+     // email: {
+       // value: '',
+       // valid: true
+     // },
+     // password: {
+      //  value: '',
+//valid: true
+   //   },
+   // }
+ // }
+  
+
+
+    return (
+      <Box
+        component="form"
+        autocomplete="off"
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+        onSubmit={ (e) => {
+          e.preventDefault();
+          console.log(email, password)
+        } }
+      >
+        <TextField
+          label="Correo electrónico"
+          variant="outlined"
+          fullWidth
+          margin="dense"
+          type="email"
+          error={false}
+          helperText={false && "Ingresa un correo electrónico válido"}
+          value= { email.value }
+          onChange={ (input) => {
+            setEmail( { value: input.target.value, valid: true } ) 
+          }
+          }
+        />
+        <TextField
+          label="Contraseña"
+          variant="outlined"
+          fullWidth
+          margin="dense"
+          type="password"
+          value= { password.value }
+          onChange={ (input) => {
+            setPassword( { value: input.target.value, valid: true } ) 
+          }
+          }
+        />
+        <Button variant="contained" type="submit">
+          Siguiente
+        </Button>
+      </Box>
+    );
+  
+}
+
+export default DatosUsuario;
